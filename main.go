aa91dc5da8976ed49cfd4dbcdc04dc4946364d86
dornascarol <carolinafdornas@gmail.com>
2025-01-16 00:44:06 -0300

    header display

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "Glô"
+	var versao float32 = 1.0
+
+	fmt.Println("Olá", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
