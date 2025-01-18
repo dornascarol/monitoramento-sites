fa4219bfeb7b7d2df398464ea9ce37c3f1a9bc0f
dornascarol <carolinafdornas@gmail.com>
2025-01-17 23:33:10 -0300

    display the menu and capture the number by scan function

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var nome string = "Glô"
-	var versao float32 = 1.0
+	name := "Glô"
+	version := 1.0
 
-	fmt.Println("Olá", nome)
-	fmt.Println("Este programa está na versão", versao)
+	fmt.Println("Hi,", name)
+	fmt.Println("This program is in version", version)
+
+	fmt.Println("1) Start website monitoring")
+	fmt.Println("2) Display the logs")
+	fmt.Println("3) Exit the program")
+
+	var command int
+	fmt.Scan(&command)
+
+	fmt.Println("The command selected was:", command)
 }
