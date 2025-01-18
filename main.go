e31eb76015a3914fcc958a857bc84a7f2d278eed
dornascarol <carolinafdornas@gmail.com>
2025-01-18 19:48:38 -0300

    perf: add flow control with if
    printing messages from commands written with the if instruction

--- main.go
+++ main.go
@@ -9,12 +9,21 @@ func main() {
 	fmt.Println("Hi,", name)
 	fmt.Println("This program is in version", version)
 
-	fmt.Println("1) Start website monitoring")
+	fmt.Println("1) Start websites monitoring")
 	fmt.Println("2) Display the logs")
 	fmt.Println("3) Exit the program")
 
 	var command int
 	fmt.Scan(&command)
-
 	fmt.Println("The command selected was:", command)
+
+	if command == 1 {
+		fmt.Println("Monitoring the websites")
+	} else if command == 2 {
+		fmt.Println("Displaying the logs")
+	} else if command == 3 {
+		fmt.Println("Exiting the program")
+	} else {
+		fmt.Println("I don't know this command")
+	}
 }
